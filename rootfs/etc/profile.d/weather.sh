66564574fc57b25b5cc31857b5b18c7ec1f80208
ufuchs <ufuchs@gmx.com>
2013-07-15 21:09:08 +0200

    added README on 'rootfs'

--- rootfs/etc/profile.d/weather.sh
+++ rootfs/etc/profile.d/weather.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# 
+# /etc/profile.d/weather.sh
+# Copyright(c) 2013 Uli Fuchs <ufuchs@gmx.com>
+# MIT Licensed
+#
+
+#
+# 1.sets the environment variables
+# 2.fetches the first weather images after system startup
+#
+
 echo "* Fetching weather"
 
 WEATHER_SERVER="xxxxxxxxxxxxxx.herokuapp.com/weather/df3120"
