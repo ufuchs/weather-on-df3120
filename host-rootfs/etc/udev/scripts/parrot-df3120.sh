d66f98614c0091f3b15ab20d4c808a40c5f739a7
ufuchs <ufuchs@gmx.com>
2013-07-21 16:55:27 +0200

    refactoring + udev rule on host

---
+++ host-rootfs/etc/udev/scripts/parrot-df3120.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# vim: syntax=sh
+
+#
+# this script 
+# 1.configures the usb0 interface
+# 2.adds an entry in the routing table
+
+INTERFACE=$1
+
+# interface
+ifconfig $INTERFACE down
+ifconfig $INTERFACE 172.16.61.1 netmask 255.255.255.0 up
+
+# firewall
+iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
+iptables --append FORWARD --in-interface $INTERFACE -j ACCEPT
+
+echo 1 > /proc/sys/net/ipv4/ip_forward	
+
