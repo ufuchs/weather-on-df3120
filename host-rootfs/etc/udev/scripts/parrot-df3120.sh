8ee905413fb7e7bbe9ea5fb66c32cbe17189e0c1
ufuchs <ufuchs@gmx.com>
2013-07-23 19:58:14 +0200

    DHCP server for 'usb0' introduced

--- host-rootfs/etc/udev/scripts/parrot-df3120.sh
+++ host-rootfs/etc/udev/scripts/parrot-df3120.sh
@@ -10,7 +10,10 @@ INTERFACE=$1
 
 # interface
 ifconfig $INTERFACE down
-ifconfig $INTERFACE 172.16.61.1 netmask 255.255.255.0 up
+ifconfig $INTERFACE 172.16.1.1 netmask 255.255.255.0 up
+
+# routing
+#route add -host 172.16.1.2 $INTERFACE
 
 # firewall
 iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
@@ -18,3 +21,4 @@ iptables --append FORWARD --in-interface $INTERFACE -j ACCEPT
 
 echo 1 > /proc/sys/net/ipv4/ip_forward	
 
+service isc-dhcp-server restart
