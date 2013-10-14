7f1d056e984d7cad836da306dc483bbbf8a6dd29
ufuchs <ufuchs@gmx.com>
2013-10-14 22:37:28 +0200

    deployment of 'icons' and 'fonts'

--- minifs/conf/packages/86weather.sh
+++ minifs/conf/packages/86weather.sh
@@ -5,10 +5,13 @@ hset sdlweather depends "libsdl libsdlimage libsdlttf"
 
 install-sdlweather() {
 	cp sdlweather "$STAGING_USR"/bin/sdlweather
+	cp -r {fonts,icons} "$STAGING_USR"/share
 	log_install echo Done
 }
 
 deploy-sdlweather() {
 	deploy cp "$STAGING_USR"/bin/sdlweather "$ROOTFS"/usr/bin
+	deploy cp -r "$STAGING_USR"/share/fonts "$ROOTFS"/usr/share
+	deploy cp -r "$STAGING_USR"/share/icons "$ROOTFS"/usr/share
 }
 
